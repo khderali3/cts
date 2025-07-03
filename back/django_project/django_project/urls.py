a0096b546aa897b920c305ee82d004926ea96dae
khderali3 <khdersliman3@gmail.com>
2025-07-04 00:38:07 +0300

    updated on 20250704 - 00:38

--- back/django_project/django_project/urls.py
+++ back/django_project/django_project/urls.py
@@ -62,3 +62,6 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if not settings.IS_PRODUCTION_ENV:
+    urlpatterns += [ path('admin/', admin.site.urls),]
